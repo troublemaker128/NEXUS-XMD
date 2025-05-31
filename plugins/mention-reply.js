f079f4c477da8589f7c0c5786ff51cc5e2b94691
⋆˚｡⋆୨✧୧˚ 𝑴𝒓 𝒘𝒊𝒔𝒆 ˚୨✧୧⋆｡˚⋆ <benardwachira49@gmail.com>
2025-06-01 00:02:10 +0300

    Update mention-reply.js

--- plugins/mention-reply.js
+++ plugins/mention-reply.js
@@ -10,16 +10,9 @@ cmd({
     if (!m.mentionedJid || m.mentionedJid.length === 0) return;
 
     const voiceClips = [
-      "https://cdn.ironman.my.id/i/7p5plg.mp4",
-      "https://cdn.ironman.my.id/i/l4dyvg.mp4",
-      "https://cdn.ironman.my.id/i/4z93dg.mp4",
-      "https://cdn.ironman.my.id/i/m9gwk0.mp4",
-      "https://cdn.ironman.my.id/i/gr1jjc.mp4",
-      "https://cdn.ironman.my.id/i/lbr8of.mp4",
-      "https://cdn.ironman.my.id/i/0z95mz.mp4",
-      "https://cdn.ironman.my.id/i/rldpwy.mp4",
-      "https://cdn.ironman.my.id/i/lz2z87.mp4",
-      "https://cdn.ironman.my.id/i/gg5jct.mp4"
+      "https://files.catbox.moe/l38fqe.mp3",
+      "https://files.catbox.moe/l38fqe.mp3",
+      
     ];
 
     const randomClip = voiceClips[Math.floor(Math.random() * voiceClips.length)];
